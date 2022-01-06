8d851d16f15c9ed4cc819c830ae41bb43b2f6830
nojuskybartas <nojus.kybartas@gmail.com>
2022-01-07 00:49:46 +0100

    added redirect func

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -7,15 +7,21 @@ export async function middleware(req) {
     // token exists if user is logged in
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    const { pathname } = req.nextUrl;
+    // this grabs the url, from which it is coming
+    const { pathname } = req.nextUrl
 
-    // allow the request if its a request for next-auth session or a token exists
-    if (pathname.includes('/api/auth') || token) {
+    // if user is already signedin, but goes to login page, redirect to home page
+    if(token && pathname === '/login'){
+        return NextResponse.redirect('/');
+    }
+
+    //if user wants to sign in
+    if(pathname.includes('/api/auth') || token){
         return NextResponse.next();
-    } 
-
-    // Redirect to login if there is no token and the user is requesting a protected route
-    if (!token && pathname !== '/login' ) {
+    }
+    
+    //redirect to login if there is no token, and user is requesting a protected route
+    if(!token && pathname !== '/login'){
         return NextResponse.redirect('/login');
     }
 }
