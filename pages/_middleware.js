2fea7678c0d48bacb67c0a02c720137a4a62c529
nojuskybartas <nojus.kybartas@gmail.com>
2022-01-07 20:12:25 +0100

    added vercel env var

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -5,7 +5,13 @@ import { NextResponse } from 'next/server';
 export async function middleware(req) {
 
     // token exists if user is logged in
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+    const token = await getToken({ 
+                                req, 
+                                secret: process.env.JWT_SECRET,
+                                secureCookie:
+                                    process.env.NEXTAUTH_URL?.startsWith("https://") ??
+                                !!process.env.VERCEL_URL, 
+                                });
 
     // this grabs the url, from which it is coming
     const { pathname } = req.nextUrl
