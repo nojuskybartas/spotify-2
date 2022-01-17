033b7c1cd487ab726bde83bfb7f4e42c428507b5
nojuskybartas <nojus.kybartas@gmail.com>
2022-01-17 18:18:12 +0100

    now only displayed to first time users

--- components/Tour.js
+++ components/Tour.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -11,20 +11,25 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '60%',
+    width: '70%',
     maxHeight: '90%',
     bgcolor: '#000',
     color: 'white',
     border: '4px solid #18D860',
-    boxShadow: 1,
-    p: 8,
     overflowY: 'scroll',
   };
 
 export default function Tour() {
-    const [visible, setVisible] = useState(true);
+  
+    const [visible, setVisible] = useState(false);
     const open = () => setVisible(true);
     const close = () => setVisible(false);
+
+    useEffect(() => {
+        const visitedBefore = JSON.parse(localStorage.getItem("visited_before"))
+        localStorage.setItem("visited_before", JSON.stringify(true))
+        setVisible(visitedBefore ? false : true)
+    }, [])
   
     return (
       <div>
@@ -42,12 +47,12 @@ export default function Tour() {
             }}
         >
             <Fade in={visible}>
-                <Box className="scrollbar-hide shadow-[0_35px_60px_-15px_rgba(100,100,100,0.3)]" sx={style}>
+                <Box className="scrollbar-hide shadow-[0_35px_60px_-15px_rgba(100,100,100,0.3)] p-4" sx={style}>
                     <Typography id="modal-modal-title" className="tour-text-title">
                     This app is a copy of the Spotify UI (api integrated)
                     </Typography>
                     <Typography id="modal-modal-description" className="tour-text" sx={{ mt: 4 }}>
-                    To actually play a song using the spotify api, you must have an instance of the original spotify app open on any device, and login with a premium account. 
+                    To actually play a song using the spotify api, you must login with a premium account (spotify api rules 🤷). 
                     </Typography>
                     <Typography id="modal-modal-description" className="tour-text" sx={{ mt: 1 }}>
                     When you click on the login button below, you will be asked to allow access to your account data. Please note that none of it is stored anywhere by this app.
